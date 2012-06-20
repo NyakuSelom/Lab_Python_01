14b9a960f99590943ead5b6128bc656f15e19931
AITI Student <aiti-student@AITI-83.(none)>
2012-06-20 11:15:18 +0000

    day 2 lab

---
+++ Day2.py
@@ -0,0 +1,43 @@
+#EX 1
+
+# No. 1
+x=4
+#56thnumber=1
+
+# No. 2
+"""   a=False
+    b= 3.7
+    c='Alex'
+    d=7
+    e='True'
+    f=17
+    g='17
+    h=True
+    i='3.14159'
+    j="---add---" """
+
+
+#EX 2
+
+#No. 1
+"""a=False
+b=True
+c=False
+
+d= b and c
+e= b or c
+f = not a and b
+g= (a and b)or not c
+h= not b and not (a or c)
+i = not ((not b or not a)and c) or a"""
+
+#EX3
+a=5/2
+b=5/2.0
+c=5.0/2
+d=7*(1/2)
+e=7*(1/2.0)
+f=5**2
+g= 5.0**2
+h=1/3
+i=1/3.0
---
+++ userinput.py
@@ -0,0 +1,11 @@
+Firstname= raw_input ('Enter Your First Name?') 
+Lastname= raw_input ('Enter Your Lastname?')
+
+DateofBirth= raw_input ('Enter Your date of birth: Day Month Year')
+
+x = DateofBirth.split
+Day= x[0]
+Month = x[1]
+Year = x[2]
+
+print Lastname,Firstname+' was born on',Month,Day,Year
