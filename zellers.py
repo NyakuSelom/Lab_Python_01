2e7aadfd3fea8b6d9b4fcde48cf44d8bfdb3f613
AITI Student <aiti-student@AITI-83.(none)>
2012-06-20 14:04:00 +0000

    day 2 lab

---
+++ zellers.py
@@ -0,0 +1,4 @@
+A= raw_input('enter the month of year where march is 1 and february is 12')
+B=raw_input('day of the month 1-31')
+C= raw_input('year of century eg 89 for 1989'/n'note if month is january or february write preceeding year ')
+D= raw_input ('centutry ')
